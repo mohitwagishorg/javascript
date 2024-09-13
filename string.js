f4373694596f637b0f3d05769f20150bb156eaea
Mohit <2021pcs5380@mnit.ac.in>
2024-09-13 07:18:38 +0530

    string finish

---
+++ string.js
@@ -0,0 +1,20 @@
+// console.log("hello" +" World "+22)
+// //Best way - backticks
+// let myname='Mohit wagish'
+// console.log(`Hello my name is ${myname.toUpperCase()}`)
+
+const GameName= new String ('Mohit');
+//if we run it on browser console-it will show like an array where e.g 0:M and we can see many methohds.console.log(GameName.toUpperCase()); //function
+
+
+
+console.log(GameName.length) //method
+// functions and method are different
+console.log(GameName.charAt(2));
+console.log(GameName.indexOf('M'))
+console.log(GameName.slice(0,3))
+
+console.log(GameName.slice(0,-1))
+let spname='jdklsh wdfj lkdc ljsnd ljkndac ljknhascloihsadfiuwf jldksc kjsbfc';
+let spn=spname.split(' ')
+console.log(spn)
