80d167b344841713802adefb9d18135d719135fb
Mohit <2021pcs5380@mnit.ac.in>
2024-09-13 07:47:05 +0530

    Math and number

---
+++ no&maths.js
@@ -0,0 +1,31 @@
+// const score=40001541.456
+// const balance= new Number(100)
+// console.log(balance);
+// console.log(score.toString().length)
+// console.log(score.toFixed(6))
+
+// console.log(score.toPrecision(12)) //returns a string
+
+// console.log(typeof(score.toPrecision(5)))
+// console.log(score.toLocaleString('en-IN'))
+
+
+// ++++++++++++++++++++++++++++++++++++++++ Maths +++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// console.log(Math)
+// console.log(Math.round(4.55))
+// console.log(Math.sqrt(25))
+// console.log(Math.pow(2,3))
+// console.log(Math.min(5,8,6,4))
+// console.log(Math.abs(-45))
+
+let a = (Math.random()*10)+1
+console.log(a)
+
+
+// for between 2 number min or max
+const min =100;
+const max =200;
+console.log(Math.floor(Math.random()*(max-min+1))+min)
+
+
